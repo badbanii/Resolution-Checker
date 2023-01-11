6fca99fc099ba7c2345ad0ae20363f7aef87d16d
Tulitu Vlad <tulituvlad@gmail.com>
2023-01-12 00:58:45 +0200

    Implemented Firebase

--- app/src/main/java/com/abetterandroid/resolutionchecker/MainActivity.kt
+++ app/src/main/java/com/abetterandroid/resolutionchecker/MainActivity.kt
@@ -4,22 +4,29 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.suddenh4x.ratingdialog.AppRating
 import com.suddenh4x.ratingdialog.preferences.RatingThreshold
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private var firebaseAnalytics: FirebaseAnalytics? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        initFirebase()
         forceDayMode()
         keepScreenAwake()
 
         if (savedInstanceState == null) {
             rateApp()
         }
+    }
+    private fun initFirebase()
+    {
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this);
     }
     private fun forceDayMode() {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
